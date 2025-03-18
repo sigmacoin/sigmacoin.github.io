458b2099e75168e9d02a6a01bd39fa921062610a
sigmacoin <avdulialbjon1@gmail.com>
2025-03-18 21:12:58 +0100

    script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.getElementById("mineButton").addEventListener("click", function() {
+document.getElementById("mineImage").addEventListener("click", function() {
     alert("Mining Sigma Coin... This may take a few moments.");
     // You can add mining logic here, such as calling an API or simulating mining.
 });
