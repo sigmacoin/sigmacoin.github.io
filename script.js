e9ee34c5a6afae78947a5a7cf0677f32abc9ece4
sigmacoin <avdulialbjon1@gmail.com>
2025-03-18 20:50:23 +0100

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-
+// script.js
+document.getElementById("joinButton").addEventListener("click", function() {
+    alert("Thank you for joining Sigma Coin! We'll redirect you to the sign-up page soon.");
+    // You can add a redirect link here:
+    // window.location.href = "https://example.com/signup";
+});
